5426c24ef65bcf0a811c04edd760418dc4556dad
Samuel <samueldivine2000@gmail.com>
2023-08-16 03:01:07 +0000

    rework

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,15 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-const map1 = list.map((x, i) => i * x);
+const list = require('./100-data.js');
+
+const multiplyByIndex = (value, index) => {
+  return value * index;
+};
+
+const newList = list.map(multiplyByIndex);
+
+console.log('The original list is:');
 console.log(list);
-console.log(map1);
+
+console.log('The new list is:');
+console.log(newList);
+
